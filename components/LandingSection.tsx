45074f299e3c3f7a15e3e57ab5d873ded9eef6d5
dan13ram <dan13ram@gmail.com>
2022-03-07 15:35:15 +0530

    feat: disabled support for smaller screens + responsive for larger screens

--- components/LandingSection.tsx
+++ components/LandingSection.tsx
@@ -16,41 +16,47 @@ export const LandingSection: React.FC<LandingSectionProps> = ({
   imageSrc,
   reversed = false,
   withButton = false,
-}) => (
-  <SimpleGrid columns={2} gap={8}>
-    <VStack
-      spacing={0}
-      my={32}
-      pl={reversed ? 0 : 16}
-      pr={reversed ? 16 : 0}
-      align="flex-start"
-    >
-      <Text fontWeight="bold" fontSize="7xl">
-        {title}
-      </Text>
-      <Text fontWeight="light" fontSize="2xl">
-        {description}
-      </Text>
-      {withButton && <JoinUsButton pt={8} />}
-    </VStack>
-    <Flex
-      pos="relative"
-      h="100%"
-      gridColumnStart={reversed ? 1 : 2}
-      gridRowStart={1}
-      _before={{
-        content: '""',
-        h: '50rem',
-        width: '250%',
-        top: '55%',
-        left: reversed ? undefined : '3rem',
-        right: !reversed ? undefined : '3rem',
-        transform: 'translateY(-45%)',
-        pos: 'absolute',
-        bg: `url(${imageSrc})`,
-        bgPos: reversed ? 'right center' : 'left center',
-        bgRepeat: 'no-repeat',
-      }}
-    />
-  </SimpleGrid>
-);
+}) => {
+  const titleFontSize = { base: '5xl', lg: '6xl', xl: '7xl' };
+  const descFontSize = { base: 'xl', xl: '2xl' };
+  const padding = { base: 8, xl: 16 };
+  return (
+    <SimpleGrid columns={2} gap={8}>
+      <VStack
+        spacing={0}
+        my={32}
+        pl={reversed ? 0 : padding}
+        pr={reversed ? padding : 0}
+        align="flex-start"
+        textAlign="left"
+      >
+        <Text fontWeight="bold" fontSize={titleFontSize}>
+          {title}
+        </Text>
+        <Text fontWeight="light" fontSize={descFontSize}>
+          {description}
+        </Text>
+        {withButton && <JoinUsButton pt={8} />}
+      </VStack>
+      <Flex
+        pos="relative"
+        h="100%"
+        gridColumnStart={reversed ? 1 : 2}
+        gridRowStart={1}
+        _before={{
+          content: '""',
+          h: '50rem',
+          width: '250%',
+          top: '55%',
+          left: reversed ? undefined : '3rem',
+          right: !reversed ? undefined : '3rem',
+          transform: 'translateY(-45%)',
+          pos: 'absolute',
+          bg: `url(${imageSrc})`,
+          bgPos: reversed ? 'right center' : 'left center',
+          bgRepeat: 'no-repeat',
+        }}
+      />
+    </SimpleGrid>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -22,80 +22,85 @@ type HomeContentAttributes = {
 
 const { title, subTitle, sections } = attributes as HomeContentAttributes;
 
-export const HomePage: React.FC = () => (
-  <>
-    <Flex
-      w="100%"
-      h="30rem"
-      _before={{
-        content: '""',
-        h: '200rem',
-        maxH: 'calc(100% - 20rem)',
-        width: '200rem',
-        top: '20rem',
-        left: '50%',
-        transform: 'translate(-50%,-50%)',
-        pos: 'absolute',
-        bg: `url(${LandingBGImage.src})`,
-        bgPos: 'center',
-        bgRepeat: 'no-repeat',
-      }}
-    />
-    <VStack w="100%" pt={8} px={24} pb={72} zIndex={1}>
-      <Text fontWeight="bold" fontSize="7xl">
-        {title}
-      </Text>
-      <Text fontWeight="light" fontSize="2xl">
-        {subTitle}
-      </Text>
-      <JoinUsButton py={8} />
-      <Flex h="12rem" />
-      <VStack spacing={32}>
-        {sections.map((section, k) => (
-          <LandingSection key={k} {...section} reversed={k % 2 === 1} />
-        ))}
-      </VStack>
+export const HomePage: React.FC = () => {
+  const titleFontSize = { base: '5xl', lg: '6xl', xl: '7xl' };
+  const descFontSize = { base: 'xl', xl: '2xl' };
+
+  return (
+    <>
       <Flex
-        h="28rem"
-        pos="relative"
+        w="100%"
+        h="30rem"
         _before={{
           content: '""',
-          h: '60rem',
-          width: '200vw',
-          top: '40rem',
+          h: '200rem',
+          maxH: 'calc(100% - 20rem)',
+          width: '200rem',
+          top: '20rem',
           left: '50%',
           transform: 'translate(-50%,-50%)',
           pos: 'absolute',
-          bg: `url(${LandingBGFooterImage.src})`,
+          bg: `url(${LandingBGImage.src})`,
           bgPos: 'center',
           bgRepeat: 'no-repeat',
-          zIndex: -1,
         }}
       />
-      <Text fontWeight="bold" fontSize="7xl">
-        {title}
-      </Text>
-      <Text fontWeight="light" fontSize="2xl">
-        {subTitle}
-      </Text>
-      <JoinUsButton py={8} />
-      <HStack>
-        {contactInfo.map(({ label, icon, url }, k) => (
-          <Link key={k} href={url} isExternal>
-            <IconButton
-              fontSize="xl"
-              borderRadius="50%"
-              aria-label={label}
-              color="black"
-              bg="white"
-              _hover={{ bg: 'ceruleanBlue' }}
-              icon={icon}
-            />
-          </Link>
-        ))}
-      </HStack>
-    </VStack>
-  </>
-);
+      <VStack w="100%" pt={8} px={24} pb={72} zIndex={1} textAlign="center">
+        <Text fontWeight="bold" fontSize={titleFontSize}>
+          {title}
+        </Text>
+        <Text fontWeight="light" fontSize={descFontSize}>
+          {subTitle}
+        </Text>
+        <JoinUsButton py={8} />
+        <Flex h="12rem" />
+        <VStack spacing={{ base: 16, xl: 32 }}>
+          {sections.map((section, k) => (
+            <LandingSection key={k} {...section} reversed={k % 2 === 1} />
+          ))}
+        </VStack>
+        <Flex
+          h="28rem"
+          pos="relative"
+          _before={{
+            content: '""',
+            h: '60rem',
+            width: '200vw',
+            top: '40rem',
+            left: '50%',
+            transform: 'translate(-50%,-50%)',
+            pos: 'absolute',
+            bg: `url(${LandingBGFooterImage.src})`,
+            bgPos: 'center',
+            bgRepeat: 'no-repeat',
+            zIndex: -1,
+          }}
+        />
+        <Text fontWeight="bold" fontSize={titleFontSize}>
+          {title}
+        </Text>
+        <Text fontWeight="light" fontSize={descFontSize}>
+          {subTitle}
+        </Text>
+        <JoinUsButton py={8} />
+        <HStack>
+          {contactInfo.map(({ label, icon, url }, k) => (
+            <Link key={k} href={url} isExternal>
+              <IconButton
+                fontSize="xl"
+                borderRadius="50%"
+                aria-label={label}
+                color="black"
+                bg="white"
+                _hover={{ bg: 'ceruleanBlue' }}
+                icon={icon}
+              />
+            </Link>
+          ))}
+        </HStack>
+      </VStack>
+    </>
+  );
+};
 
 export default HomePage;
