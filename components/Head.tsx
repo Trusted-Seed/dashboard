ef60013726db8d3e503087c290f41af7ab39e02c
dan13ram <dan13ram@gmail.com>
2022-04-01 15:35:32 +0530

    fixed netlify-identity-widget

--- components/Head.tsx
+++ components/Head.tsx
@@ -9,11 +9,13 @@ export const Head: React.FC<HeadProps> = ({
   children,
   title = 'Trusted Seed',
 }) => (
-  <NextHead>
-    <title>{title}</title>
-    <meta charSet="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <>
+    <NextHead>
+      <title>{title}</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      {children}
+    </NextHead>
     <NextScript src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
-    {children}
-  </NextHead>
+  </>
 );
