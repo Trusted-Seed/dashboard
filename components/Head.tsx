16bfa711a32096367b0cc28959115f4cf6a701c8
dan13ram <dan13ram@gmail.com>
2022-05-23 18:32:25 +0530

    added HubSpot

--- components/Head.tsx
+++ components/Head.tsx
@@ -1,4 +1,5 @@
 import NextHead from 'next/head';
+import NextScript from 'next/script';
 
 type HeadProps = {
   title?: string;
@@ -8,10 +9,19 @@ export const Head: React.FC<HeadProps> = ({
   children,
   title = 'Trusted Seed',
 }) => (
-  <NextHead>
-    <title>{title}</title>
-    <meta charSet="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    {children}
-  </NextHead>
+  <>
+    <NextHead>
+      <title>{title}</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      {children}
+    </NextHead>
+    <NextScript // HubSpot
+      type="text/javascript"
+      id="hs-script-loader"
+      async
+      defer
+      src="//js-na1.hs-scripts.com/8132714.js"
+    />
+  </>
 );
